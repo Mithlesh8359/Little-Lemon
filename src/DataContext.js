5b9d1800a7b4bd1f0e2a2e5eada95bcb2e5b951e
Mithlesh Chandrakar <91368429+Mithlesh8359@users.noreply.github.com>
2024-11-03 04:36:12 +0530

    Delete src directory

--- src/DataContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react"; // Correct import statement
-
-const DataContext = createContext({}); // Create the context
-export const DataContextProvider = DataContext.Provider; // Export the Provider component
-export default DataContext; // Export the context itself
